a3325a58d2ae02b64b3b2b9e6bb3980857af1592
nneonx008-debug <nneonx008@gmail.com>
2026-01-31 16:52:38 +0100

    Update vehicle.js

--- models/vehicle.js
+++ models/vehicle.js
@@ -19,11 +19,18 @@ const  vehicleSchema  = new Schema({
     extraimg:String  ,
     secondimg:String  ,
     thirdimg:String , 
-      boosting:{
-    type:Number ,
-    default : 0 
-  } ,
+     expired:{type: Boolean , default:false} , 
+    deadline : {type : Date , default : null} , 
+    deleteconfirm: {type:Boolean , default:false} , 
           VIP: { type: Boolean, default: false }, // 👈 new f
+   createdAt: { type: Date, default: Date.now },
+  adExpiresAt: { type: Date },        // When ad stops being visible
+  vipExpiresAt: { type: Date , default:null },
+    boostCount : {type: Number ,  default: 0},
+    views : {type : Number , default : 0} ,
+    names: {type:String} , 
+    isFirst :{type : Boolean ,default : false },
+  boost: { type: Date , default:null }, // When VIP boost ends
 
     userId:{type : mongoose.Schema.Types.ObjectId , ref:'User'}
 })
