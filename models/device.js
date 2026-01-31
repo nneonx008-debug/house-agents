231e90106478ee078111acbfa0cee9500eb4d5cd
nneonx008-debug <nneonx008@gmail.com>
2026-01-31 16:49:36 +0100

    Update device.js

--- models/device.js
+++ models/device.js
@@ -17,14 +17,20 @@ const deviceSchema = new Schema({
     extraimg:String , 
     secondimg:String , 
     phonenumber:String ,
-      boosting:{
-    type:Number ,
-    default : 0 
-  } ,
     thirdimg:String  ,
     
-      VIP: { type: Boolean, default: false }, // 👈 new f
-    userId:{type:mongoose.Schema.Types.ObjectId , ref:'User'}
-} ,{timestamps:true}) ; 
+    expired:{type: Boolean , default:false} , 
+    deadline : {type : Date , default : null} , 
+    deleteconfirm: {type:Boolean , default:false} , 
+          VIP: { type: Boolean, default: false }, // 👈 new f
+   createdAt: { type: Date, default: Date.now },
+  adExpiresAt: { type: Date },        // When ad stops being visible
+  vipExpiresAt: { type: Date , default:null },
+    boostCount : {type: Number ,  default: 0},
+    views : {type : Number , default : 0} ,
+    names: {type:String} , 
+    isFirst :{type : Boolean ,default : false },
+  boost: { type: Date , default:null }, // When VIP boost ends,
+      userId:{type:mongoose.Schema.Types.ObjectId , ref:'User'}} ,{timestamps:true}) ; 
 module.exports = mongoose.model('Devices' , deviceSchema) ; 
 
