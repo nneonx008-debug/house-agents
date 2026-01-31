e1aacf43495f2f237b57c97bb7b557fb647816cb
nneonx008-debug <nneonx008@gmail.com>
2026-01-31 16:50:45 +0100

    Update furniture.js

--- models/furniture.js
+++ models/furniture.js
@@ -14,15 +14,21 @@ const furniture  =  new Schema({
 
     phonenumber:String ,
     bgimg:String , 
-      boosting:{
-    type:Number ,
-    default : 0 
-  } ,
     extraimg:String , 
     userId:{type:mongoose.Schema.Types.ObjectId , ref:'User'} , 
     secondimg:String , 
+    expired:{type: Boolean , default:false} , 
+    deadline : {type : Date , default : null} , 
+    deleteconfirm: {type:Boolean , default:false} , 
           VIP: { type: Boolean, default: false }, // 👈 new f
-
+   createdAt: { type: Date, default: Date.now },
+  adExpiresAt: { type: Date },        // When ad stops being visible
+  vipExpiresAt: { type: Date , default:null },
+    boostCount : {type: Number ,  default: 0},
+    views : {type : Number , default : 0} ,
+    names: {type:String} , 
+    isFirst :{type : Boolean ,default : false },
+  boost: { type: Date , default:null }, // When VIP boost ends
     thirdimg:String 
     
 })
