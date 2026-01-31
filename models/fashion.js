2d9676d3b18a146d73ae44ee1df1a598630c11bd
nneonx008-debug <nneonx008@gmail.com>
2026-01-31 16:50:07 +0100

    Update fashion.js

--- models/fashion.js
+++ models/fashion.js
@@ -16,13 +16,19 @@ const fashionSchema = new Schema({
     extraimg:String,
     location:String  , 
     thirdimg:String  , 
-      boosting:{
-    type:Number ,
-    default : 0 
-  } ,
     color:String  ,
+    expired:{type: Boolean , default:false} , 
+    deadline : {type : Date , default : null} , 
+    deleteconfirm: {type:Boolean , default:false} , 
           VIP: { type: Boolean, default: false }, // 👈 new f
-
+   createdAt: { type: Date, default: Date.now },
+  adExpiresAt: { type: Date },        // When ad stops being visible
+  vipExpiresAt: { type: Date , default:null },
+    boostCount : {type: Number ,  default: 0},
+    views : {type : Number , default : 0} ,
+    names: {type:String} , 
+    isFirst :{type : Boolean ,default : false },
+  boost: { type: Date , default:null }, // When VIP boost ends
     gender:String  ,
     userId:{type:mongoose.Schema.Types.ObjectId , ref:'User'}
 }) ; 
