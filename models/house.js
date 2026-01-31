93ddc3d2404a0ee0951c00f7b445a94b6f1e52fe
nneonx008-debug <nneonx008@gmail.com>
2026-01-31 16:51:39 +0100

    Update house.js

--- models/house.js
+++ models/house.js
@@ -12,24 +12,30 @@ const  housesSchema =  new Schema({
 
   description:String ,  
     specific :String , 
-
+ 
     price :String , 
     phonenumber:String ,
     created:{type : Date,
         default:Date.now
     } , 
     beds : Number  ,
-    baths :Number ,
-        typo : String ,
-
-  comments: [{ 
-    text: String, 
-    date: { type: Date, default: Date.now } 
-  }],
+    baths :Number , 
+    typo : String ,
     category : String , 
-    typo:String , 
     location :String  ,
+    expired:{type: Boolean , default:false} , 
+    deadline : {type : Date , default : null} , 
+    deleteconfirm: {type:Boolean , default:false} , 
           VIP: { type: Boolean, default: false }, // 👈 new f
+   createdAt: { type: Date, default: Date.now },
+  adExpiresAt: { type: Date },        // When ad stops being visible
+  vipExpiresAt: { type: Date , default:null },
+    boostCount : {type: Number ,  default: 0},
+    views : {type : Number , default : 0} ,
+    names: {type:String} , 
+    isFirst :{type : Boolean ,default : false },
+  boost: { type: Date , default:null } // When VIP boost ends
+
 
 })
 
